68ff503ac160d13d82036627766ef62a0fbe67b5
Nicolas DELAUNEY <niko-d@live.fr>
2022-10-20 18:00:06 +0200

    🚨 [settings] lint credits

--- cmd/settings/credits.go
+++ cmd/settings/credits.go
@@ -4,13 +4,13 @@ import "github.com/charmbracelet/lipgloss"
 
 var (
 	loveStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
-	urlStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("35"))
+	urlStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("35"))
 )
 
 func Credits() string {
 	return `
 Open Source license (MIT)
-Code with ` + loveStyle.Render("♥️") + ` in Golang. 
+Coded with ` + loveStyle.Render("♥️") + ` in Golang. 
 
 Copyright (c) 2022 Nicolas Delauney
 More info on ` + urlStyle.Render("https://github.com/delni/goodmorning") + "."
