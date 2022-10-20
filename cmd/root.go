03b29c2b8db85f249180ae73f936a565af6cfaed
Nicolas DELAUNEY <niko-d@live.fr>
2022-10-20 11:44:07 +0200

    🎉 Minimal CLI with Cobra

---
+++ cmd/root.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "goodmorning",
+	Short: "Goodmorning helps you automate you morning routine",
+	Long: `A Fast and Flexible morning routine automater built with BubbleTea from Charm.`,
+	Run: func(cmd *cobra.Command, args []string) {
+	  fmt.Println("Goodmorning")
+	},
+  }
+  
+  func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+	  fmt.Println(err)
+	  os.Exit(1)
+	}
+  }
+  
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "goodmorning/cmd"
+
+func main() {
+	cmd.Execute()
+  }
